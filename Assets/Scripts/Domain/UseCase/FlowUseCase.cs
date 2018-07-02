53b2fec37c33736bcd3beb22c809743202c7c8e2
mattak <mattak.me@gmail.com>
2018-07-02 15:06:45 +0900

    issues/2 :recycle: Divide interface and implementation file in UseCase

--- Assets/Scripts/Domain/UseCase/FlowUseCase.cs
+++ Assets/Scripts/Domain/UseCase/FlowUseCase.cs
@@ -11,39 +11,6 @@ using UniRx;
 
 namespace CAFU.Flow.Domain.UseCase
 {
-    public interface IFlowUseCase : IUseCase
-    {
-        /// <summary>
-        /// Loads FlowModels from FlowEntityList
-        /// </summary>
-        void Load();
-
-        /// <summary>
-        /// Start flow
-        /// </summary>
-        void Start();
-
-        /// <summary>
-        /// Stop flow
-        /// </summary>
-        void Stop();
-
-        /// <summary>
-        /// Resume flow
-        /// </summary>
-        void Resume();
-
-        /// <summary>
-        /// Pause flow
-        /// </summary>
-        void Pause();
-
-        /// <summary>
-        /// Flow of Ids.
-        /// </summary>
-        UniRx.IObservable<int> IdFlowAsObservable { get; }
-    }
-
     public class FlowUseCase : IFlowUseCase
     {
         public class Factory : DefaultUseCaseFactory<FlowUseCase>
