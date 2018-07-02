9ebf89c6b4390b7d815ed3d6d7201db99d712ac2
mattak <mattak.me@gmail.com>
2018-07-03 01:15:12 +0900

    feat/#5 :seedling: Define IMultipleModelFlowPresenter

---
+++ Assets/Scripts/Presentation/Presenter/IMultipleModelFlowPresenter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CAFU.Core.Presentation.Presenter;
+using CAFU.Flow.Domain.UseCase;
+
+namespace CAFU.Flow.Presentation.Presenter
+{
+    public interface IMultipleModelFlowPresenter : IPresenter
+    {
+        IMultipleModelFlowUseCase MultipleModelFlowUseCase { get; }
+    }
+
+    public static class IMultipleModelFlowPresenterExtension
+    {
+        public static void LoadMultipleFlow(this IMultipleModelFlowPresenter presenter)
+        {
+            presenter.MultipleModelFlowUseCase.Load();
+        }
+
+        public static void StartMultipleFlow(this IMultipleModelFlowPresenter presenter, string key)
+        {
+            presenter.MultipleModelFlowUseCase.Start(key);
+        }
+
+        public static void StopMultipleFlow(this IMultipleModelFlowPresenter presenter, string key)
+        {
+            presenter.MultipleModelFlowUseCase.Stop(key);
+        }
+
+        public static void PauseMultipleFlow(this IMultipleModelFlowPresenter presenter, string key)
+        {
+            presenter.MultipleModelFlowUseCase.Pause(key);
+        }
+
+        public static void ResumeMultipleFlow(this IMultipleModelFlowPresenter presenter, string key)
+        {
+            presenter.MultipleModelFlowUseCase.Resume(key);
+        }
+
+        public static IObservable<TModel> GetModelMultipleFlow<TModel>(this IMultipleModelFlowPresenter presenter, string key)
+        {
+            return presenter.MultipleModelFlowUseCase.GetModelFlowAsObservable<TModel>(key);
+        }
+    }
+}
