7914e9803c44d17e46510e370d1bfbaacb672ce3
mattak <mattak.me@gmail.com>
2018-07-03 01:02:54 +0900

    feat/#5 :seedling: Define MultipleModelFlowUseCase

---
+++ Assets/Scripts/Domain/UseCase/IMultipleModelFlowUseCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CAFU.Core.Domain.Model;
+
+namespace CAFU.Flow.Domain.UseCase
+{
+    public interface IMultipleModelFlowUseCase : IMultipleFlowUseCase
+    {
+        IObservable<TModel> GetModelFlowAsObservable<TModel>(string key);
+        void LoadModel<TModel>(IDictionary<int, TModel> modelMap);
+    }
+}
---
+++ Assets/Scripts/Domain/UseCase/MultipleModelFlowUseCase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CAFU.Core.Domain.Model;
+using CAFU.Core.Domain.UseCase;
+using UniRx;
+
+namespace CAFU.Flow.Domain.UseCase
+{
+    public class MultipleModelFlowUseCase :
+        MultipleFlowUseCase,
+        IMultipleModelFlowUseCase
+    {
+        public new class Factory : DefaultUseCaseFactory<MultipleModelFlowUseCase>
+        {
+            protected override void Initialize(MultipleModelFlowUseCase instance)
+            {
+                base.Initialize(instance);
+                instance.Initialize();
+            }
+        }
+
+        public IDictionary<string, IDictionary<int, IModel>> Key2Id2ModelMap { get; set; }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            this.Key2Id2ModelMap = new Dictionary<string, IDictionary<int, IModel>>();
+        }
+
+        public void LoadModel<TModel>(IDictionary<int, TModel> modelMap)
+        {
+            var typeKey = typeof(TModel).FullName;
+            this.Key2Id2ModelMap[typeKey] = modelMap.ToList().ToDictionary(it => it.Key, it => it.Value as IModel);
+        }
+
+        public IObservable<TModel> GetModelFlowAsObservable<TModel>(string key)
+        {
+            var typeKey = typeof(TModel).FullName;
+            return base.GetIdFlowAsObservable(key).Select(id => this.Key2Id2ModelMap[typeKey][id]).Cast<IModel, TModel>();
+        }
+    }
+}
