763ffea90292cb7fada6023792b1ebba9f98381c
mattak <mattak.me@gmail.com>
2018-07-03 00:59:50 +0900

    feat/#5 :seedling: Define MultipleFlowUseCase

---
+++ Assets/Scripts/Domain/UseCase/IMultipleFlowUseCase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CAFU.Core.Domain.UseCase;
+
+namespace CAFU.Flow.Domain.UseCase
+{
+    public interface IMultipleFlowUseCase : IUseCase
+    {
+        /// <summary>
+        /// Load object
+        /// </summary>
+        /// <param name="key">key of flow</param>
+        void Load();
+
+        /// <summary>
+        /// Start flow
+        /// </summary>
+        /// <param name="key">key of flow</param>
+        void Start(string key);
+
+        /// <summary>
+        /// Stop flow
+        /// </summary>
+        /// <param name="key">key of flow</param>
+        void Stop(string key);
+
+        /// <summary>
+        /// Resume flow
+        /// </summary>
+        /// <param name="key">key of flow</param>
+        void Resume(string key);
+
+        /// <summary>
+        /// Pause flow
+        /// </summary>
+        /// <param name="key">key of flow</param>
+        void Pause(string key);
+
+        /// <summary>
+        /// Get id flow by key
+        /// </summary>
+        /// <param name="key">key of flow</param>
+        /// <returns></returns>
+        IObservable<int> GetIdFlowAsObservable(string key);
+    }
+}
